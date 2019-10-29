c0c395fab48b34c6c753cd69d0baf67808a2e3cf
LCN29 <1455476411@qq.com>
2019-10-29 21:09:00 +0800

    配置文件读取

--- controller/src/main/java/com/kpe/ApplicationLauncher.java
+++ controller/src/main/java/com/kpe/ApplicationLauncher.java
@@ -5,9 +5,9 @@ import io.vertx.core.Launcher;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-  public class Application extends Launcher {
+public class ApplicationLauncher extends Launcher {
 
     public static void main(String[] args) {
-      new Application().dispatch(new String[] { "run", EngineVerticle.class.getName() });
+      new ApplicationLauncher().dispatch(new String[] { "run", EngineVerticle.class.getName() });
     }
 }
