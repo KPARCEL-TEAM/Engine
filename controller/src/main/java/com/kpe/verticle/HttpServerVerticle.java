7764e42a286e7094c2b72790e7c56ed29715e1a2
LCN29 <1455476411@qq.com>
2019-10-30 10:24:41 +0800

    httpServer 提交

---
+++ controller/src/main/java/com/kpe/verticle/HttpServerVerticle.java
@@ -0,0 +1,11 @@
+package com.kpe.verticle;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * @description:
+ * @author: LCN
+ * @date: 2019-10-30 10:24
+ */
+public class HttpServerVerticle extends AbstractVerticle {
+}
